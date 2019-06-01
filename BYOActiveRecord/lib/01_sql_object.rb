419d02782adf3de1c2b1855e94de9bfd24fd762d
mwdeshaw <mwdeshaw@gmail.com>
2019-05-31 17:07:14 -0700

    Work on BYOActiveRecord

--- BYOActiveRecord/lib/01_sql_object.rb
+++ BYOActiveRecord/lib/01_sql_object.rb
@@ -4,39 +4,72 @@ require 'active_support/inflector'
 # of this project. It was only a warm up.
 
 class SQLObject
+
   def self.columns
-    # ...
+    @columns ||= DBConnection.execute2("select * from #{table_name}")
+    @columns.first.map { |ele| ele.to_sym }
   end
 
   def self.finalize!
+    self.columns.each do |col|
+
+      define_method("#{col}") do
+        self.attributes[col]
+      end
+
+      define_method("#{col}=") do |val|
+        self.attributes[col] = val
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table = table_name
   end
 
   def self.table_name
-    # ...
+    @table ||= self.to_s.tableize
   end
 
   def self.all
-    # ...
+    table = self.table_name
+    result = DBConnection.execute("select * from #{table}")
+    self.parse_all(result)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map do |ele|
+      self.new(ele)
+    end
   end
 
   def self.find(id)
-    # ...
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{table_name}.id
+      FROM
+        #{table_name}
+      WHERE
+        #{table_name}.id = ?
+    SQL
+
+    self.parse_all(result).first
   end
 
   def initialize(params = {})
-    # ...
+    obj = self.class
+    params.each do |attr_name, v|
+      if obj.columns.include?(attr_name.to_sym)
+        send(attr_name.to_s + '=', v)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
+
   end
 
   def attributes
-    # ...
+    @attributes||= {}
   end
 
   def attribute_values
@@ -44,7 +77,7 @@ class SQLObject
   end
 
   def insert
-    # ...
+
   end
 
   def update
